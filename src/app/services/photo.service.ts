e6ddd5561987329771980d444a4fb81c5aa5dd9e
Skyler Greenman <sky.todd.green@gmail.com>
2020-07-15 11:12:25 -0600

    fixed ng-for array bug

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -18,22 +18,45 @@ export class PhotoService {
 
   public photos: Photo [] = [];
 
-  public format: string
+  public format: string;
+  public webPath: string;
+  private PHOTO_STORAGE_KEY: string = "photo";
+
+  JSONobj = {
+    name: "mike",
+    weight: 150
+  }
+  imageArray: object [] = [];
+
+
+  Console(obj: string){
+    console.log("Printing WebPath: " + obj);
+  }
 
   async TakePhoto () { 
-    try{
       const metadata = await this.OpenCameraAndTakePhoto();    
       const base64Photo = await this.FetchPhoto(metadata);
-      this.WritePhotoToLocalStorage(base64Photo);
-    }
-    catch(error){
-      this.LogError(error)
-    }
-    // Promise Chaining - I can't figure out how to get this work - it doesn't seem to be passing the values to each upcoming function
-    // this.OpenCameraAndTakePhoto()
-    // .then(this.FetchPhoto)
-    // .then(this.WritePhotoToLocalStorage)
-    // .catch(this.LogError)
+      const imageObj = await this.WritePhotoToLocalStorage(base64Photo);
+      this.photos.unshift(imageObj);
+
+      console.log("photos Array after I take picture: " +  JSON.stringify(this.photos))
+      
+      Storage.set({
+        key: this.PHOTO_STORAGE_KEY,
+        value: JSON.stringify(this.photos)
+      })
+  }
+
+  async loadPhotosFromStorage(){
+    console.log("JSONobj: " + this.JSONobj)
+    console.log("stringify(JSONobj): " + JSON.stringify(this.JSONobj))
+    
+    const photos = await Storage.get({
+      key: this.PHOTO_STORAGE_KEY
+    });
+    console.log("photos.value: " + JSON.parse(photos.value));
+    
+    this.photos = JSON.parse(photos.value) || [];
   }
 
   async OpenCameraAndTakePhoto(){
@@ -44,6 +67,7 @@ export class PhotoService {
       source: CameraSource.Camera
     })
     this.format = photoMetadata.format;
+    this.webPath = photoMetadata.webPath;
     console.log('photoMetadata:' + JSON.stringify(photoMetadata))
     console.log('photoMetadata.webPath:' + JSON.stringify(photoMetadata.webPath))
     return photoMetadata;
@@ -51,7 +75,10 @@ export class PhotoService {
 
   async FetchPhoto(photoMetadata){
     const responseObj = await fetch(photoMetadata.webPath);
-    
+    //debugger;
+
+    // if web, fetch DataURL(?)
+
     // Print out fetch results. To do so, we need to re-fetch so we don't lock the body stream (?)
     await fetch(photoMetadata.webPath).then(response => {
       response.text()
@@ -59,6 +86,9 @@ export class PhotoService {
           console.log("fetched response: " + text)
         })
     })
+
+    // If hybrid, readFile (instead of fetch)
+      // Todo
     
     const blob = await responseObj.blob();
     console.log('photoBlob:' + JSON.stringify(blob));
@@ -79,16 +109,22 @@ export class PhotoService {
     const date = new Date();
     const time = date.getTime();
     const path = time + this.format;
+    
+    //debugger;
 
     Filesystem.writeFile({
       path: path,
-      data: base64Photo
+      data: base64Photo,
+      directory: FilesystemDirectory.Data
     })
 
     console.log("File Written: " + JSON.stringify(path),
       JSON.stringify(base64Photo)
     )
-    // if hybrid, readFile instead of fetch DataURL(?)
+    return({
+      path: path,
+      webPath: this.webPath
+    })
   }
   
   convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
@@ -101,17 +137,16 @@ export class PhotoService {
 
   });
 
-  LogError(error){
-    console.log('%c Error Occurred: ', 'background: #FF0000; color: #00000' + error);
+  LogError(err){
+    console.error('%c Error Occurred: ', 'background: #FF0000; color: #00000' + err.stack);
   }
   
-
 }
 
 class Photo {
   path: String  //hybrid only?
   webPath: String
-  base64: String
+  base64?: String
 }
 
 
